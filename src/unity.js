334212961baf382ae608b7febf153c960dae220a
Hiếu Nguyễn <nghuuhieu1994@gmail.com>
2024-06-08 15:53:43 +0700

    Update unity.js

--- src/unity.js
+++ src/unity.js
@@ -5,7 +5,7 @@ module.exports = { createManualActivationFile, activateManualLicense, activateSe
 
 async function activateSerialLicense(unityPath, username, password, serial) {
     // use '-projectPath ?' for skipping project indexing
-    const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -projectPath "?" -username "${username}" -password "${password}" -serial "${serial}"`);
+    const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -username "${username}" -password "${password}" -serial "${serial}"`);
     if (!stdout.includes('Next license update check is after')) {
         throw new Error('Activation failed');
     }
